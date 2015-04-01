f08a61dbf4c0bb3b81d4589f2ff89ca087a8e541
mlagran1 <mlagran1>
2015-04-01 15:18:38 -0400

    testing commit

---
+++ trainer.h
@@ -0,0 +1,21 @@
+#ifndef TRAINER_H
+#define TRAINER_H
+
+using namespace std;
+
+class Trainer {
+
+	public:
+		void pick_move;	//allows trainer to select move in battle
+		void pick_pokemon;	//allows trainer to select pokemon in battle
+		void poke_stats;	//allows trainer to check stats in battle
+		void switch_pokemon;	//allows trainer to switch order of pokemon
+
+
+	private:
+		Pokemon starter[6];
+		vector<Pokemon> All_poke
+		
+};
+
+#endif
